c94cd9e86c72f1086d22d81df1344e520b5bfe31
kmstrube81 <kasey@kmstrube.net>
2025-10-17 16:33:32 -0500

    Update format.js

--- src/format.js
+++ src/format.js
@@ -66,7 +66,7 @@ function buildEmbed(template = {})
 		embed.setDescription(description);
 	//set thumbnail
 	if(thumbnail)
-		embed.setThumbnail(thumbnail);
+		embed.setThumbnail(thumbnail.uri);
 	//set fields
 	if(fields)
 		fields.map((f, i) => {
@@ -114,7 +114,7 @@ export function editEmbed(embed, template = {}, mode = "edit")
 	}
 	//set thumbnail
 	if(thumbnail)
-		embed.setThumbnail(thumbnail);
+		embed.setThumbnail(thumbnail.uri);
 	//set fields
 	if(fields){
 		if(mode !== "append"){
